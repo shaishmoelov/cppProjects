ea600ddc10e1c9652e5faea494509f025083128a
SHAI SHMAILOV <SHAY4@rafael.co.il>
2023-06-06 20:53:39 +0300

    replace memory allocate with alignment 16 bit

--- sumOfImages/sumOfImages/main.cpp
+++ sumOfImages/sumOfImages/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <new>
+
+
+namespace std {
+    enum class align_val_t : size_t;
+};
+void* operator new(std::size_t, std::align_val_t);	// new overload
+
 
 // Image properties
 const int width = 1024;
@@ -8,25 +16,24 @@ const int height = 768;
 const int bit_depth = 10;
 
 // Function to load a binary image
-std::vector<uint16_t> loadBinaryImage(const std::string& filename)
+void * loadBinaryImage(const std::string& filename)
 {
     FILE* file = fopen(filename.c_str(), "r");
     if (!file)
     {
         std::cerr << "Failed to open file: " << filename << std::endl;
-        return std::vector<uint16_t>();
+        return nullptr;
     }
+    void* image = new (std::align_val_t(16)) short[width * height * 2];
 
-    std::vector<uint16_t> image(width * height);
-
-    fread(image.data(), sizeof(uint16_t), width * height, file);
+    fread(image, sizeof(uint16_t), width * height * 2, file);
     fclose(file);
 
     return image;
 }
 
 // Function to save a binary image
-void saveBinaryImage(const std::string& filename, const std::vector<uint16_t>& image)
+void saveBinaryImage(const std::string& filename, void * image)
 {
     FILE* file = fopen(filename.c_str(), "wb");
     if (!file)
@@ -35,40 +42,47 @@ void saveBinaryImage(const std::string& filename, const std::vector<uint16_t>& i
         return;
     }
 
-    fwrite(image.data(), sizeof(uint16_t), width * height, file);
+    fwrite(image, sizeof(uint16_t), width * height, file);
     fclose(file);
 }
 
 // Function to add two images and return the result
-std::vector<uint16_t> addImages(const std::vector<uint16_t>& image1, const std::vector<uint16_t>& image2)
+//
+void * addImages(void *image1, void *image2)
 {
-    std::vector<uint16_t> result(width * height);
+    void *result = new (std::align_val_t(32)) short[width * height * 2];
 
-    for (int i = 0; i < width * height; ++i)
+    for (int i = 0; i < width * height * 2; ++i)
     {
-        result[i] = std::min(image1[i] + image2[i], static_cast<int>((1 << bit_depth) - 1));
+        //result[i] = image1[i] + image2[i] + 1;
     }
 
     return result;
 }
-
+/*assign array with maloc or new - new with alignment */
 int main()
 {
-    std::string file1 = "G:\\My Drive\\myProject\\pythonProjectss\\pythonProjectss\\createImages\\image1.bin";
-    std::string file2 = "G:\\My Drive\\myProject\\pythonProjectss\\pythonProjectss\\createImages\\image2.bin";
+    std::string file1 = "G:/My Drive/myProject/pythonProjectss/pythonProjectss/createImages/image_1_with_noise.bin";
+    std::string file2 = "G:/My Drive/myProject/pythonProjectss/pythonProjectss/createImages/image_2_with_noise.bin";
     std::string outputFile = "sum_of_images.bin";
 
     // Load the binary images
-    std::vector<uint16_t> image1 = loadBinaryImage(file1);
-    std::vector<uint16_t> image2 = loadBinaryImage(file2);
+    //change to arrays with align malloc or new
+    /*std::vector<uint16_t> image1 = loadBinaryImage(file1);
+    std::vector<uint16_t> image2 = loadBinaryImage(file2);*/
 
-    if (image1.empty() || image2.empty())
+    void * image1 = loadBinaryImage(file1);
+    void * image2 = loadBinaryImage(file2);
+
+    if (!image1 || !image2)
     {
         return 1;
     }
 
     // Add the images
-    std::vector<uint16_t> sumImage = addImages(image1, image2);
+    //array of sum
+    void* sumImage = new (std::align_val_t(16)) short[width * height * 2];
+    sumImage = addImages(image1, image2);
 
     // Save the resulting image
     saveBinaryImage(outputFile, sumImage);
@@ -77,3 +91,5 @@ int main()
 
     return 0;
 }
+
+//take from matlab 2 images - google
